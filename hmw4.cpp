d61e54430b94549190e8dfbe7678b15c214489ec
lukabrown <117334114+lukabrown@users.noreply.github.com>
2023-03-10 15:29:36 -0600

    cleaned up code

--- hmw4.cpp
+++ hmw4.cpp
@@ -3,13 +3,12 @@
 
 #include <queue>
 #include <vector>
-#include <functional>
-#include <utility>
 #include <iostream>
 #include <cmath>
+#include <cstdio>
 #include <cstdlib>
 #include <iomanip>
-#include <random>
+//#include <random> //for c++ 11 exponential number generator
 
 struct Process {
     double serviceTime;
@@ -36,36 +35,33 @@ static const int ERROR = 5000; //generates excess processes for scheduling near 
 static const int MAX_PROCESSES = 10000 + ERROR;
 
 //prototypes
+static void   Init(int argc, char *argv[], std::vector<Process> &processes, Processor &CPU);
 static void   FCFS(std::vector<Process> &processes, Processor &CPU);
-static void   Results(std::vector<Process> &processes, Processor &CPU, double arrivalRate, double serviceRate);
-static void   Init(int argc, char *argv[], double &arrivalRate, double &serviceRate, std::vector<Process> &processes, Processor &CPU);
+static void   Results(std::vector<Process> &processes, Processor &CPU);
 static double Poisson(double lambda);
-static double Exponential(double lambda); //needs more work
 
 int main(int argc, char *argv[]) {
 
     //global variables
-    std::vector<Process> processes(MAX_PROCESSES); //list of 10000 processes
+    std::vector<Process> processes(MAX_PROCESSES); //list of >=10000 processes
     Processor CPU;                                 //holds simulation data
-    double arrivalRate, serviceRate;               //input variables
 
     //functions
-    Init(argc, argv, arrivalRate, serviceRate, processes, CPU); //initializes and seeds random
-    FCFS(processes, CPU);                                       //computes simulation
-    Results(processes, CPU, arrivalRate, serviceRate);          //reports sim results
+    Init(argc, argv, processes, CPU);              //initializes and seeds random
+    FCFS(processes, CPU);                          //computes simulation
+    Results(processes, CPU);                       //reports sim results
     return 0;
 }
 
 //simulates FCFS CPU scheduler
 static void FCFS(std::vector<Process> &processes, Processor &CPU) {
     std::priority_queue<Process> min_queue;
-
+    
     while (CPU.processesCompleted != MAX_PROCESSES-ERROR) {
         //updates time to next process picked, and picks the process
         if (CPU.idle) { //finds a process
 
             if (min_queue.size() == 0) { //waits for next process in future
-                //finds next future process
                 int i;
                 for (i = CPU.processesCompleted; i < MAX_PROCESSES; i++) {
                     if (CPU.time < processes[i].arrivalTime) {
@@ -110,7 +106,7 @@ static void FCFS(std::vector<Process> &processes, Processor &CPU) {
 }
 
 //prints useful data to screen
-static void Results(std::vector<Process> &processes, Processor &CPU, double arrivalRate, double serviceRate) {
+static void Results(std::vector<Process> &processes, Processor &CPU) {
     //computes avg turnaround
     double turnaround = processes[0].finishTime - processes[0].arrivalTime;
     for (int i = 1; i < CPU.processesCompleted; i++) {
@@ -118,7 +114,7 @@ static void Results(std::vector<Process> &processes, Processor &CPU, double arri
         turnaround /= 2.0;
     }
 
-    //prints all output stats
+    //outputs all stats
     std::cout << std::fixed << std::setprecision(2);
     std::cout << "\nFinished FCFS simulation.\n";
     std::cout << CPU.processesCompleted << " processes completed in " << CPU.time << " s\n";
@@ -126,22 +122,31 @@ static void Results(std::vector<Process> &processes, Processor &CPU, double arri
     std::cout << "Average turnaround time:     " << turnaround << " s\n";
     std::cout << "Throughput:                  " << (MAX_PROCESSES-ERROR)/CPU.time << " processes/s\n";
     std::cout << "CPU Utilization:             " << (CPU.time-CPU.idleTime)/CPU.time*100 << " %\n";
-    //std::cout << "Idle: " << CPU.idleTime/CPU.time*100 << " %, " << CPU.idleTime << " s\n";
-
     //debug //for (int i = 10000; i <= 10000; i++) {std::cout << i << ' ' << processes[i].finishTime << ' ' << processes[i].serviceTime << '\n';}
 }
 
 //initializes all variables
-static void Init(int argc, char *argv[], double &arrivalRate, double &serviceRate, std::vector<Process> &processes, Processor &CPU) {
-    //inits input vars
-    if (argc != 3) {
-        std::cout << "ERROR: Parameters: Arrival_Rate Service_Rate\n";
-        return;
+static void Init(int argc, char *argv[], std::vector<Process> &processes, Processor &CPU) {
+    //parses input vars
+    if (argc != 3) { //throws error for not correct # of parameters
+        fprintf(stderr,"ERROR: Parameters: Arrival_Rate Service_Rate\n");
+        exit(-1);
     }
-    arrivalRate = atof(argv[1]);
-    serviceRate = 1 / atof(argv[2]);
-    std::cout << "Parameters: Arrival_Rate: " << arrivalRate << " processes/s\n";
-    std::cout << "            Service_Rate: " << 1 / serviceRate << " s\n";
+
+    //reads variables
+    double arrivalRate = atof(argv[1]);
+    double serviceRate = atof(argv[2]);
+
+    if (arrivalRate <= 0) { //checks arrival rate
+        fprintf(stderr,"ERROR: Arrival_Rate must be > 0\n");
+        exit(-1);
+    }
+
+    if (serviceRate <= 0 || serviceRate > 1) { //checks service rate
+        fprintf(stderr,"ERROR: 0 < Service_Rate <= 1\n");
+        exit(-1);
+    } else
+        serviceRate = 1 / serviceRate; //corrects service rate
 
     //initializes processor
     CPU.idle = true;
@@ -151,17 +156,14 @@ static void Init(int argc, char *argv[], double &arrivalRate, double &serviceRat
     CPU.idleTime = 0;
 
     //seeds random
-    unsigned int seed = time(NULL);
-    //seed = 1;
+    unsigned int seed = time(NULL); //seed = 1678403118; //for debug, random seed
     srand(seed);
+
+    //outputs program parameters
+    std::cout << "Parameters: Arrival_Rate: " << arrivalRate << " processes/s\n";
+    std::cout << "            Service_Rate: " << 1 / serviceRate << " s\n";
     std::cout << "            Seed:         " << seed << '\n';
-    
-    /*for c++ 11
-    //exponential number generator
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::exponential_distribution<> d(serviceRate);*/
-    
+
     //initializes processes
     for (int i = 0; i < MAX_PROCESSES; i++) {
         if (i == 0)
@@ -170,26 +172,17 @@ static void Init(int argc, char *argv[], double &arrivalRate, double &serviceRat
             processes[i].arrivalTime = Poisson(arrivalRate) + processes[i-1].arrivalTime;
 
         processes[i].serviceTime = Poisson(serviceRate);
-        //processes[i].serviceTime = d(gen); //for c++ 11
         processes[i].finishTime = -1; //-1 = unfinished
     }
 }
 
-// returns a random number that follows an exponential distribution
+//returns a random number that follows an exponential distribution
 static double Poisson(double lambda) {
 
-	double temp, num = 0;
-	while (num == 0 || num == INFINITY) {
+    double temp, num = 0;
+    while (num == 0 || num == INFINITY) {
         temp = (double) rand() / RAND_MAX; //0 or 1
         num = (-1 / lambda) * log(temp); 
-	}
-	return(num);
-}
-
-//supposed to generate nums on exp dist.
-static double Exponential(double lambda) {
-    double temp = lambda * exp(-lambda * rand());
-    while (temp == INFINITY || temp == 0)
-        temp = lambda * exp(-lambda * (double) rand());
-    return temp;
+    }
+    return num;
 }
